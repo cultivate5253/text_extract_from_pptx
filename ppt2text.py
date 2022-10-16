3164d2d8c5134646799fe40192c9add04ea86a96
Cultivate <30587888+cultivate5253@users.noreply.github.com>
2022-10-16 13:38:59 +0900

    Add files via upload

---
+++ ppt2text.py
@@ -0,0 +1,34 @@
+
+import pptx
+from glob import glob
+
+#for fname in glob ('*.pptx'):
+#print ('File name: ', fname, '\n')
+   
+def ppt2text(fname):
+    prs = pptx.Presentation(fname)
+
+    for i, sld in enumerate(prs.slides, start=1):
+
+        print(f'-- Page {i} --')
+
+        for shp in sld.shapes:
+
+            if shp.has_text_frame:
+                print (shp.text)
+
+            if shp.has_table:
+                tbl = shp.table
+                row_count = len(tbl.rows)
+                col_count = len(tbl.columns)
+                for r in range(0, row_count):                 
+                    text=''
+                    for c in range(0, col_count):
+                        cell = tbl.cell(r,c)
+                        paragraphs = cell.text_frame.paragraphs 
+                        for paragraph in paragraphs:
+                            for run in paragraph.runs:
+                                text+=run.text
+                            text+=', '
+                    print (text)
+            print ()
